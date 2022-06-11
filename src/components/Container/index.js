e63dd3cc6fc90143a5ba2dae02f3d09f1d2f42c1
Nick Melanson <nickm890@gmail.com>
2022-06-11 01:12:51 -0400

    added components, pages, basic nav

---
+++ src/components/Container/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Projects from "../pages/Projects";
+import Navigation from "../Navigation";
+
+const Container = () => {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Projects") {
+      return <Projects />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      {renderPage()}
+    </div>
+  );
+};
+
+export default Container;
